0dd8a4fa589fd38ada4ba54c5d55544342b73a48
Nehuen-R <nehuenroth72@gmail.com>
2025-08-20 17:48:01 -0300

    Initial Commit

---
+++ ChallengeMELI_Nehuen_Roth/ChallengeMELI_Nehuen_RothApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeMELI_Nehuen_RothApp.swift
+//  ChallengeMELI_Nehuen_Roth
+//
+//  Created by nehuen roth on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeMELI_Nehuen_RothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
